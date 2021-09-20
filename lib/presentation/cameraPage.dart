9fb246b3772a7818bbad543c6265eaab04794682
mipsmonsta <yaojintham@gmail.com>
2021-09-20 15:19:01 +0800

    Added camera preview and camera buttons; text recognition not done

--- lib/presentation/cameraPage.dart
+++ lib/presentation/cameraPage.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CameraPage extends StatefulWidget {
   final List<CameraDescription> cameras;
@@ -12,6 +13,8 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
   late CameraController _controller;
+  bool _controllerInitialized = false;
+  String _textToShow = "";
 
   @override
   void initState() {
@@ -19,54 +22,80 @@ class _CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
     print(widget.cameras);
     WidgetsBinding.instance?.addObserver(this);
     _controller = CameraController(widget.cameras[0], ResolutionPreset.medium);
-    _controller.initialize().then((_) {
+    _controller?.initialize().then((_) {
       if (!mounted) {
         return;
       }
-      setState(() {});
+      setState(() => _controllerInitialized = true);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]); // only allow portrait mode for screen
     return Scaffold(
+        backgroundColor: Colors.black,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.camera_alt),
+          onPressed: () {
+
+            
+            setState(() => _textToShow = "Camera Btn Pressed!");
+          },
+        ),
         appBar: AppBar(title: Text("Camera Preview")),
-        body: (!_controller.value.isInitialized)
-            ? Container(child: Center(child: Text("Camera not ready!")))
-            : CameraPreview(_controller));
+        body: LayoutBuilder(builder: (ctx, constraint) {
+          //print("$constraint.maxHeight, $constraint.width");
+
+          return (!_controllerInitialized)
+              ? Center(
+                  child: CircularProgressIndicator(
+                  semanticsLabel: "Camera Initializing",
+                ))
+              : SizedBox(
+                  width: constraint.maxWidth,
+                  height: constraint.maxHeight,
+                  child: Column(children: [
+                    AspectRatio(
+                        child: CameraPreview(_controller),
+                        aspectRatio: 1.0 / _controller.value.aspectRatio),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                          child: Text(_textToShow,
+                              style: TextStyle(color: Colors.white))),
+                    )
+                  ]),
+                );
+        }));
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     // App state changed before we got the chance to initialize.
-    if (_controller == null || !_controller.value.isInitialized) {
+    if (!_controller.value.isInitialized) {
       return;
     }
     if (state == AppLifecycleState.inactive) {
-      _controller?.dispose();
+      _controller.dispose();
+      setState(() => _controllerInitialized = false);
     } else if (state == AppLifecycleState.resumed) {
-      if (_controller != null) {
-        onNewCameraSelected(_controller.description);
-      }
+      onNewCameraSelected(_controller.description);
     }
   }
 
   void onNewCameraSelected(CameraDescription cameraDescription) async {
-    if (_controller != null) {
-      await _controller.dispose();
-    }
+    await _controller.dispose();
+    setState(() => _controllerInitialized = false);
+
     _controller = CameraController(
       cameraDescription,
       ResolutionPreset.medium,
     );
-
-    // If the controller is updated then update the UI.
-    _controller.addListener(() {
-      if (mounted) setState(() {});
-      if (_controller.value.hasError) {
-        print("${_controller.value.errorDescription}");
-      }
-    });
 
     try {
       await _controller.initialize();
@@ -75,7 +104,9 @@ class _CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
     }
 
     if (mounted) {
-      setState(() {});
+      setState(() {
+        _controllerInitialized = true;
+      });
     }
   }
 
@@ -83,6 +114,9 @@ class _CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
   void dispose() {
     WidgetsBinding.instance?.removeObserver(this);
     _controller.dispose();
+    setState(() {
+      _controllerInitialized = false;
+    });
     super.dispose();
   }
 }
