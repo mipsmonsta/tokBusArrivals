526bf394644de667d5fd5928548467cf465808d5
mipsmonsta <yaojintham@gmail.com>
2021-09-27 23:14:10 +0800

    Release 0.9 App Bundle signing

--- lib/main.dart
+++ lib/main.dart
@@ -64,19 +64,19 @@ class MyApp extends StatelessWidget {
                     .state)), // put above MaterialAppLevel so that mute state read/write app-wide
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Tok Bus Arrival',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.lightGreen),
         routes: {
           '/': (_) => ArrivalsMainPage(),
           '/settings': (_) => SpeechSettingsPage(),
